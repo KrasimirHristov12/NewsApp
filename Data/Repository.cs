1d47b64bb0dcaf95068d997811b5db80e03c35ed
Krasimir Hristov <hkrasimir53@gmail.com>
2022-10-08 19:22:40 +0300

    Initial Commit

---
+++ Data/Repository.cs
@@ -0,0 +1,31 @@
+﻿namespace NewsApp.Data
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext dbContext;
+        public Repository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IQueryable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
